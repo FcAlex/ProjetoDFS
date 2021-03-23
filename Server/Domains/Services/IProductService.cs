294a1d62267b243d6befa2aff643266c06f2f749
FcAlex <contato.fcoalex@gmail.com>
2021-03-23 16:55:54 -0300

    Add Product repository and service classes

---
+++ Server/Domains/Services/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Server.Communication;
+using Server.Domains.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Domains.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> ListAllAsync();
+        Task<Product> FindById(int id);
+        Task<Response<Product>> SaveAsync(Product product);
+        Task<Response<Product>> UpdateAsync(int id, Product product);
+        Task<Response<Product>> DeleteAsync(int id);
+    }
+}
