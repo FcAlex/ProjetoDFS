ebfa7f16db319cb6da46df0ea38146dace2deff6
FcAlex <contato.fcoalex@gmail.com>
2021-06-06 22:06:31 -0300

    add orders

--- frontend/web/src/pages/Orders/index.jsx
+++ frontend/web/src/pages/Orders/index.jsx
@@ -1,8 +1,52 @@
 
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import api from '../../services/api'
+import useAuth from '../../hooks/useAuth'
+import { useEffect } from 'react'
+
 const Order = props => {
+
+  const { getData } = useAuth()
+  const { id } = getData()
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await api.get("/purchase")
+        
+        console.log(response)
+      } catch (error) {
+        console.log(error.message)
+      }
+    })()
+  }, [id])
+
   return (
     <main>
-      
+      <h1>Compras Realizadas</h1>
+      <div className="listaCompras">
+        <div style={{ display: 'flex', alignItems: "center" }}>  {/* TODO */}
+          <Input placeholder="Busque por ID" />
+          <Button bg="green" icon="filter" />
+        </div>
+
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Valor Total</th>
+              <th>Data</th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody>
+
+          </tbody>
+        </table>
+      </div>
     </main>
   )
 }
